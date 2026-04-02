8d06853208f1a528ea0495b50cbd24e029315e19
Hajy <hajy@MacBook-Air-Hajy.local>
2026-04-02 19:58:13 +0500

    first commit with folder and main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import uvicorn
+import requests
+
+app = FastAPI()
+
+
+
+@app.get('/')
+def home():
+    a = requests.get('http://178.20.45.13:8000/ru/product/get/all')
+    b = a.json()
+    return b
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",   # файл:переменная
+        host="0.0.0.0",
+        port=8000,
+        reload=True   # авто-перезапуск при изменениях (для разработки)
+    )
